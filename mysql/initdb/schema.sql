ea55e0560ce10389556ab498c484c69233cb25c7
matumoto1234 <matumotosizuoka@gmail.com>
2023-09-08 17:44:55 +0900

    add default current_timestamp

--- mysql/initdb/schema.sql
+++ mysql/initdb/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE `articles` (
   `title` varchar(255) NOT NULL,
   `body` varchar(255) NOT NULL,
   `position` json NOT NULL,
-  `created_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
 
@@ -11,7 +11,7 @@ CREATE TABLE `threads` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `position` json NOT NULL,
-  `created_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
 
@@ -20,7 +20,7 @@ CREATE TABLE `comments` (
   `article_id` BIGINT NOT NULL,
   `body` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   INDEX `article_id_index` (`article_id`)
 );
