c92888e287897ebf7ab3b2080d7068e770cf8ffc
Keyl0ve <kyo.cat34@gmail.com>
2023-09-14 16:10:09 +0900

    modify: init db data string -> svg

--- mysql/initdb/01_schema.sql
+++ mysql/initdb/01_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `articles` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `body` varchar(255) NOT NULL,
+  `body` TEXT NOT NULL,
   `position` json NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -8,7 +8,7 @@ CREATE TABLE `articles` (
 
 CREATE TABLE `threads` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
+  `title` TEXT NOT NULL,
   `position` json NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -16,7 +16,7 @@ CREATE TABLE `threads` (
 
 CREATE TABLE `keijiban_rakugaki` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `body` VARCHAR(255) NOT NULL,
+  `body` TEXT NOT NULL,
   `position` json NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
@@ -26,7 +26,7 @@ CREATE TABLE `keijiban_rakugaki` (
 CREATE TABLE `thread_rakugaki` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `thread_id` BIGINT NOT NULL,
-  `body` VARCHAR(255) NOT NULL,
+  `body` TEXT NOT NULL,
   `position` json NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
