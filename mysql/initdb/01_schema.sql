7957f2c94671d2d4e8ff9fbc401dbca8b0342b02
Keyl0ve <kyo.cat34@gmail.com>
2023-09-14 11:22:34 +0900

    delete: comment table

--- mysql/initdb/01_schema.sql
+++ mysql/initdb/01_schema.sql
@@ -15,16 +15,6 @@ CREATE TABLE `threads` (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `comments` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `thread_id` BIGINT NOT NULL,
-  `body` varchar(255) NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  INDEX `thread_id_index` (`thread_id`)
-);
-
 CREATE TABLE `keijiban_rakugaki` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `body` VARCHAR(255) NOT NULL,
--- mysql/initdb/02_init.sql
+++ mysql/initdb/02_init.sql
@@ -14,14 +14,6 @@ INSERT INTO `threads` (`title`, `position`) VALUES
   ('Thread 4', '{"x": 20, "y": 25}'),
   ('Thread 5', '{"x": 25, "y": 30}');
 
--- comments テーブルにデータを挿入
-INSERT INTO `comments` (`thread_id`, `body`, `author`) VALUES
-  (1, 'Comment 1 for Thread 1', 'Author 1'),
-  (1, 'Comment 2 for Thread 1', 'Author 2'),
-  (2, 'Comment 1 for Thread 2', 'Author 3'),
-  (3, 'Comment 1 for Thread 3', 'Author 4'),
-  (4, 'Comment 1 for Thread 4', 'Author 5');
-
 INSERT INTO `keijiban_rakugaki` (`body`, `position`) VALUES
   ('svg 1', '{"x": 5, "y": 10}'),
   ('svg 2', '{"x": 10, "y": 15}'),
